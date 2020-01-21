80d41035b1741882a6b503e598bfe4df444c7406
Nitin <nitin.kumar@dbdigital.in>
2020-01-21 19:08:01 +0530

    Added UIView+Extension file.

---
+++ NAExtensions/NAExtensions/UIView+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Extension.swift
+//  NAExtensions
+//
+//  Created by Nitin A on 21/01/20.
+//  Copyright © 2020 Nitin A. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func makeCircle() {
+        layer.cornerRadius = min(frame.size.height, frame.size.width) * 0.5
+        layer.masksToBounds = true
+        clipsToBounds = true
+    }
+}
+
